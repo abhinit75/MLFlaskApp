5c8b8989257d5c984e7a6db4156345ebcd69bb20
Abhinit Patil <abhineet0309@gmail.com>
2020-05-25 11:50:56 -0400

    Added all files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=abhiai/mlflaskapp
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+docker login
+kubectl run $dockerpath:latest --image=abhiai/mlflaskapp --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get deployments
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/prediction-pod 8000:80
